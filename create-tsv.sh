332c8d8f0de5c2bb566e2185ebf17d635ca9f293
UriShX <usdogi@gmail.com>
2021-01-10 19:50:04 +0200

    ch name, curl then jq

---
+++ create-tsv.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl --silent "https://api.nordvpn.com/v1/servers/groups" | jq --raw-output '.[]  | [.id, .identifier, .title] | @tsv' >server-groups.tsv
+
+curl --silent "https://api.nordvpn.com/v1/servers/countries" | jq --raw-output '.[] | [.id, .code, .name] | @tsv' >countries.tsv
+sed -i 's/\ /_/g' countries.tsv
