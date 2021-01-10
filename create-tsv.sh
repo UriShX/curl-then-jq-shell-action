3dce643edc1948aba0a70d6ee331bc4fd4feec8f
UriShX <usdogi@gmail.com>
2021-01-10 20:40:08 +0200

    create dump file

--- create-tsv.sh
+++ create-tsv.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+curl --silent "https://api.nordvpn.com/v1/servers" >dump.json
+
 curl --silent "https://api.nordvpn.com/v1/servers/groups" | jq --raw-output '.[]  | [.id, .identifier, .title] | @tsv' >server-groups.tsv
 
 curl --silent "https://api.nordvpn.com/v1/servers/countries" | jq --raw-output '.[] | [.id, .code, .name] | @tsv' >countries.tsv
