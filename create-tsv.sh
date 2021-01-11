c1b9c81e68487ea9f9ba9ba8517870bb6118998f
UriShX <usdogi@gmail.com>
2021-01-12 00:02:01 +0200

    Add titles, fix replacing spaces w/ underscore

--- create-tsv.sh
+++ create-tsv.sh
@@ -2,7 +2,8 @@
 
 curl --silent "https://api.nordvpn.com/v1/servers" >dump.json
 
-curl --silent "https://api.nordvpn.com/v1/servers/groups" | jq --raw-output '.[]  | [.id, .identifier, .title] | @tsv' >server-groups.tsv
+echo 'ID Identifier Title' | sed 's/\ /\t/g' >server-groups.tsv
+curl --silent "https://api.nordvpn.com/v1/servers/groups" | jq --raw-output '.[]  | [.id, .identifier, .title] | @tsv' | sed 's/\ /_/g' >>server-groups.tsv
 
-curl --silent "https://api.nordvpn.com/v1/servers/countries" | jq --raw-output '.[] | [.id, .code, .name] | @tsv' >countries.tsv
-sed -i 's/\ /_/g' countries.tsv
+echo 'ID Code Name' | sed 's/\ /\t/g' >countries.tsv
+curl --silent "https://api.nordvpn.com/v1/servers/countries" | jq --raw-output '.[] | [.id, .code, .name] | @tsv' | sed 's/\ /_/g' >>countries.tsv
