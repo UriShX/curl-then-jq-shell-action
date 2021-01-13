8cd8680d7700e57850565b0acc427ff83cd8bb76
UriShX <usdogi@gmail.com>
2021-01-13 21:13:28 +0200

    Fix missing first lines

--- create-tsv.sh
+++ create-tsv.sh
@@ -4,7 +4,7 @@ GROUPS_DATA=$(curl --silent "https://api.nordvpn.com/v1/servers/groups" | jq --r
 COUNTRIES_DATA=$(curl --silent "https://api.nordvpn.com/v1/servers/countries" | jq --raw-output '.[] | [.id, .code, .name] | @tsv' | sed 's/\ /_/g')
 
 function identifiers() {
-    echo "$1" | while read y; do
+    cat "$1" | while read y; do
         echo $(awk '{print $2}')
     done
 }
@@ -12,23 +12,26 @@ function identifiers() {
 ### RUN ###
 rm group-countries.tsv server-groups.tsv countries.tsv
 touch group-countries.tsv server-groups.tsv countries.tsv
-IDS=$(identifiers "$GROUPS_DATA")
 printf "ID\tIdentifier\tTitle\n" >>server-groups.tsv
+echo "$GROUPS_DATA" >>server-groups.tsv
 printf "ID\tCode\tName\n" >>countries.tsv
+echo "$COUNTRIES_DATA" >>countries.tsv
 printf "Group Identifier\tCountries\n" >>group-countries.tsv
+IDS=$(identifiers server-groups.tsv)
 for X in $IDS; do
     COUNTRIES=$(curl --silent "https://api.nordvpn.com/v1/servers?filters\[servers_groups\]\[identifier\]=$X&filters\[servers_technologies\]\[identifier\]=openvpn_tcp" | jq --raw-output '[.[] | .locations | .[].country.code] | unique | @tsv' | sed 's/\t/,\ /g')
     if [ ! -z "$COUNTRIES" ]; then
         printf "$X\t$COUNTRIES\n" >>group-countries.tsv
-        echo "$GROUPS_DATA" | grep "$X" >>server-groups.tsv
+    else
+        sed -i "/$X/d" server-groups.tsv
     fi
 done
 
-CODES=$(identifiers "$COUNTRIES_DATA")
+CODES=$(identifiers countries.tsv)
 
 for X in $CODES; do
     N=$(grep -w "$X" group-countries.tsv)
-    if [ ! -z "$N" ]; then
-        echo "$COUNTRIES_DATA" | grep "$X" >>countries.tsv
+    if [ -z "$N" ]; then
+        sed -i "/$X/d" countries.tsv
     fi
 done
